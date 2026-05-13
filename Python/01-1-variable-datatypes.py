c7c2fad79eb3be4a885b6d8b18ab73aac8443053
datasciencekibaatein <dhruvtechsutra@gmail.com>
2026-05-13 11:18:39 +0530

    updated files

---
+++ Python/01-1-variable-datatypes.py
@@ -0,0 +1,55 @@
+#Variable
+num = 34
+print(num)
+num = 45
+print(num)
+
+name = "Dhruv"
+print(name)
+
+
+#Datatypes
+
+#Numeric ( int, float, complex)
+#Text/String (whatever you will pass in double quotes"")
+#boolean (True/False)
+#None (it represent no value)
+
+
+#Integer
+age = 22
+
+print(age)
+
+print(type(age))
+
+#float
+
+rating = 3.4
+
+print(rating)
+print(type(rating))
+
+#complex
+cmp = 2 + 3j
+
+print(cmp)
+print(type(cmp))
+
+
+#String
+name = "Dhruv Verma"
+print(name)
+print(type(name))
+
+
+#Boolean
+
+indicator = False
+print(indicator)
+print(type(indicator))
+
+#None
+val = None
+print(val)
+print(type(val))
